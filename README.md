7f8b7821ef3ca901cfb7ff82a29204bbcd5f13f3
fuge <houyichaochao@gmail.com>
2022-03-12 11:43:07 +0800

    更新readme 和获取公共ip逻辑

--- README.md
+++ README.md
@@ -2,17 +2,30 @@
 Security needs to delete AK, SK and other key information
 
 ## How to run? 
+
+### Step1 
+Install docker and run service
+
+ubuntu: https://www.runoob.com/docker/ubuntu-docker-install.html
+
+win: https://www.runoob.com/docker/windows-docker-install.html
+
+mac: https://www.runoob.com/docker/macos-docker-install.html
+
+### Step2
 Download the latest version of the client in the release
 
-### mac&linux
+### Step3 
+
+#### mac&linux
 In terminal run:
 
 go_mac_provider_amd64 --username=xxxx --password=xxxx --cpu=1 --ram=1
 
 go_linux_provider_amd64 --username=xxxx --password=xxxx --cpu=1 --ram=1
 
-### win
+#### win
 In cmd run:
 
-go_linux_provider_amd64 --username=xxxx --password=xxxx --cpu=1 --ram=1
+start go_linux_provider_amd64 --username=xxxx --password=xxxx --cpu=1 --ram=1
 
--- utils/common.go
+++ utils/common.go
@@ -6,6 +6,7 @@ import (
 	"github.com/forge1yc/clog/clog"
 	"os/exec"
 	"regexp"
+	"runtime"
 	"strconv"
 	"strings"
 )
@@ -30,40 +31,51 @@ func MergeStdOutAndErrOut(stOut,erOut *bytes.Buffer) string {
 // @Date: 2021/11/29 15:10
 func PingCheck(ip string) bool {
 
-	c := exec.Command("ping", "-t", "2", ip)
-	//c := exec.Command("ping", "-t", "3", "39.104.50.89")
+	sysType := runtime.GOOS
+	var (
+		c *exec.Cmd
+	)
+	// windows系统
+	if sysType == "windows" {
+		c = exec.Command("ping", "-n", "2", ip)
+		return systemPingCheck(ip, c)
+	}
 
+	// mac系统
+	if sysType == "darwin" {
+		c = exec.Command("ping", "-t", "2", ip)
+	} else {
+		c = exec.Command("ping", "-c", "2", ip)
+	}
+
+	return systemPingCheck(ip,c)
+}
+
+func systemPingCheck(ip string, c *exec.Cmd) bool {
 	result, err := c.CombinedOutput()
+	//clog.Info("ping result: %v",string(result))
 
 	if err != nil {
-		//fmt.Printf("%+v\n",err)
-		clog.Error("combined output %v error: %v",ip,err)
-		return false
-	}
-	if strings.Contains(string(result),"100.0% packet loss") {
-		//fmt.Printf("%+v\n","100% loss")
+		clog.Error("combined output %v error: %v", ip, err)
 		return false
 	}
 
-	pingResult := regexp.MustCompile(`time=(.*) ms`)
+	if strings.Contains(string(result), "100.0% packet loss") {
+		return false
+	}
+
+	pingResult := regexp.MustCompile(`.*=(.*)(.*ms)`)
 	params := pingResult.FindStringSubmatch(string(result))
-	delay, err := strconv.ParseFloat(params[1], 32)
+	delay, err := strconv.ParseFloat(strings.TrimSpace(params[1]), 32)
 	if err != nil {
 		clog.Error("get delay error, will set to private ip")
 		return false
 	}
-	if delay >= 1 {
-		return false
-	}
-
-
-	if strings.Contains(string(result),"0.0% packet loss") {
-		//fmt.Printf("%+v\n","100% loss")
+	if delay < 1 {
 		return true
 	}
 
 	return false
-
 }
 
 // ConvertStrSliceToIntSlice 转换
